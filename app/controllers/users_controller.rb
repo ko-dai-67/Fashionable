d802ddddd6d2f53239a9a1e8e09a8c4d247e780a
kodai.iwashita <back.together0607@gmail.com>
2021-03-03 20:11:39 +0000

    users_show_fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  
+   before_action :authenticate_user!, only: [:show]
   
  def show
    @user = User.find_by(id: params[:id])
+   @posts = current_user.posts.order('created_at DESC')
  end
 end
