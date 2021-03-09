b3fd43acfefb7a01e953e6991234280fdcf20060
kodai.iwashita <back.together0607@gmail.com>
2021-03-09 17:31:29 +0000

    follow view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   before_action :authenticate_user!, only: [:show, :likes]
+   before_action :authenticate_user!, only: [:show, :likes, :followings, :followers]
   
  def show
    @user = User.find(params[:id])
@@ -10,6 +10,18 @@ class UsersController < ApplicationController
  def likes
    @user = User.find(params[:id])
    @likeposts = @user.likeposts.order('created_at DESC')
+   counts(@user)
+ end
+ 
+ def followings
+   @user = User.find(params[:id])
+   @followings = @user.followings
+   counts(@user)
+ end
+ 
+ def followers
+   @user = User.find(params[:id])
+   @followers = @user.followers.order('created_at DESC')
    counts(@user)
  end
 end
